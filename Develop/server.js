131de85b57c255ed38ecd0d5eec76c6f10cb6509
Karen Pion <karenlpion@gmail.com>
2021-07-23 23:31:19 -0400

    get requests for html and api content

---
+++ Develop/server.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const app =  express();
+const PORT = 3000;
+
+const notesContent = require('./db/db.json')
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Get route for homepage
+app.get('/', (req,res)=>
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+);
+
+// Get route for notes
+app.get('/notes', (req,res)=>
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => 
+  res.json(notesContent));
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
